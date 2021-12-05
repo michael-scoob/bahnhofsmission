a93d282c3cf0f8a195f76dd5ce38b4d143d9b3b2
Your Name <you@example.com>
2021-12-05 13:36:22 +0100

    download button hinzugefügt

--- statistik.py
+++ statistik.py
@@ -13,16 +13,35 @@ def app():
     day=str(datetime.date.day)
     today = datetime.date.today()
     
+    # Datum auswahl
     sd = st.date_input("Start Datum wählen", today,key="sd")
     ed = st.date_input("End Datum wählen", today,key="ed")
 
+    db = database()
+    data = db.getAllData() #methode return a df 
+    
+    # Download button
+    @st.cache
+    def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    csv = convert_df(data)
+
+    st.download_button(
+        label="Download",
+        data=csv,
+        file_name=str(today) + '.csv',
+        mime='text/csv',
+    )
     
     # st.title('Demo Daten')
     # chart_data = pd.DataFrame(  
     # np.random.randn(20, 3),
     # columns=['a', 'b', 'c'])
     # st.line_chart(chart_data)
-
+    
+    #  
     if st.button("Daten anschauen!",key="dw"):
         db = database()
         db_data = db.getAllData()
