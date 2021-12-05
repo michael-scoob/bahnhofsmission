914ebe3f01babffc65ee692ef4c604aca74ac469
Your Name <you@example.com>
2021-12-05 12:27:37 +0100

    start und end datum in statistik hinzugefügt

--- statistik.py
+++ statistik.py
@@ -8,8 +8,14 @@ from backend.database import database
 def app():
     st.title('Statistik')
     st.write('Auswertung der Daten über auszuwählende Zeiträume. Bitte einen Zeitraum wählen!')
-    d = st.date_input("Datum wählen", datetime.date(2019, 7, 6))
-    st.write('Ausgewältes Datum ist:', d)
+    year=str(datetime.date.year)
+    month=str(datetime.date.month)
+    day=str(datetime.date.day)
+    today = datetime.date.today()
+    
+    sd = st.date_input("Start Datum wählen", today,key="sd")
+    ed = st.date_input("End Datum wählen", today,key="ed")
+
     
     # st.title('Demo Daten')
     # chart_data = pd.DataFrame(  
@@ -17,7 +23,7 @@ def app():
     # columns=['a', 'b', 'c'])
     # st.line_chart(chart_data)
 
-    if st.button("Daten anschauen!",key=d):
+    if st.button("Daten anschauen!",key="dw"):
         db = database()
         db_data = db.getAllData()
         st.table(db_data)
